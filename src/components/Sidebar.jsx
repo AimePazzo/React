2df221418336b4c96eafab1dee3bff94c81c555e
AimePazzo <ndagijimanapazo64@gmail.com>
2022-08-27 14:15:32 -0700

    second commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import {Sishopware} from 'react-icons/si';
+import {Sishopware} from 'react-icons/Si';
 import {MdOutlineCancel} from 'react-icons/md';
 import {TooltipComponent} from '@sysncfusion/ej2-react-popups';
 import {Links} from '../data/dummy';
